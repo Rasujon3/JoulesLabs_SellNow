1346949687503e0e2d41dcb5d3d644f7fc4a7776
Rasujon3 <rasujon3@gmail.com>
2026-01-13 23:58:24 +0600

    add DTO to enforce add-to-cart data contract

--- src/Application/Cart/AddToCartDTO.php
+++ src/Application/Cart/AddToCartDTO.php
@@ -1,3 +1,21 @@
 <?php
 
 declare(strict_types=1);
+
+namespace App\Application\Cart;
+
+final class AddToCartDTO
+{
+    public function __construct(
+        public int $productId,
+        public int $quantity
+    ) {
+        if ($this->productId <= 0) {
+            throw new \InvalidArgumentException('Invalid product ID');
+        }
+
+        if ($this->quantity <= 0) {
+            throw new \InvalidArgumentException('Quantity must be positive');
+        }
+    }
+}
